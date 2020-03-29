44a469b8e56de5e1a574d798588c7b1eafdb1cd4
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-03-29 02:04:06 -0300

    Registers 'health_bp' blueprint

--- kamui/application.py
+++ kamui/application.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
+from kamui.entrypoints.rest.health import health_bp
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(health_bp)
 
     return app
