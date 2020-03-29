be4afae03fdd1dcd66a9fe853fe55e8aec99a006
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-03-29 12:57:07 -0300

    Creates 'web_home_bp' blueprint and register it into the application

--- kamui/application.py
+++ kamui/application.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
 from kamui.entrypoints.rest.health import health_bp
+from kamui.entrypoints.web.home import web_home_bp
 
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(health_bp)
+    app.register_blueprint(web_home_bp)
 
     return app
---
+++ kamui/entrypoints/web/home/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+
+web_home_bp = Blueprint("web_home", __name__, template_folder="templates")
