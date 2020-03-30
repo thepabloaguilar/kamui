ae9ec3c82a0d1ab23632ee21cb91ddfd1c84314c
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-03-29 23:19:24 -0300

    Register classes in Dependency Injection Container

--- kamui/configuration/dependency_injection.py
+++ kamui/configuration/dependency_injection.py
@@ -1,3 +1,24 @@
 from punq import Container
 
+from kamui.core.usecase.project.create_new_project import (
+    CreateNewProjectUsecase,
+    CreateNewProject,
+)
+from kamui.core.usecase.project.get_projects_list import (
+    GetProjectsListUsecase,
+    GetProjectsList,
+)
+from kamui.dataproviders.database.project.repository import (
+    CreateNewProjectRepository,
+    GetProjectsListRepository,
+)
+
 di_container = Container()
+
+# Dependencies
+di_container.register(CreateNewProject, CreateNewProjectRepository)
+di_container.register(GetProjectsList, GetProjectsListRepository)
+
+# Usecases
+di_container.register(CreateNewProjectUsecase)
+di_container.register(GetProjectsListUsecase)
