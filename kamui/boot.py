564d666c416cc47b00cc3570453f61eb88feda6d
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-03-29 22:27:40 -0300

    Adds 'initdb' command into the flask CLI

--- kamui/boot.py
+++ kamui/boot.py
@@ -1,6 +1,13 @@
 from kamui.application import create_app
+from kamui.configuration.database import init_db
 
 application = create_app()
+
+
+@application.cli.command("initdb")
+def _init_db():
+    init_db()
+
 
 if __name__ == "__main__":
     application.run()
