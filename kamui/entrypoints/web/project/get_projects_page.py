974b9363ddcd75b7ec1e6d8c5adee0834e12c532
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-03-29 23:30:06 -0300

    Modifies GetProjectsPage to get list of all created projects

--- kamui/entrypoints/web/project/get_projects_page.py
+++ kamui/entrypoints/web/project/get_projects_page.py
@@ -1,10 +1,17 @@
 from flask import render_template
 from flask.views import View
 
+from kamui.core.usecase.project.get_projects_list import GetProjectsListUsecase
+from kamui.configuration.dependency_injection import di_container
+
 
 class GetProjectsPage(View):
     PATH = "/projects"
     methods = ["GET"]
 
+    def __init__(self):
+        self.__get_projects_list_usecase = di_container.resolve(GetProjectsListUsecase)
+
     def dispatch_request(self):
-        return render_template("projects_page.html", projects=[])
+        projects = self.__get_projects_list_usecase()
+        return render_template("projects_page.html", projects=projects)
