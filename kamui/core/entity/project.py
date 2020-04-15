574c7206956f146be057b35d6a626e7370be8987
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-14 23:13:16 -0300

    Removes the "id" attribute from "Project" class and changes the "project_key" attribute to "project_id"

--- kamui/core/entity/project.py
+++ kamui/core/entity/project.py
@@ -7,8 +7,7 @@ from .project_status import ProjectStatus
 
 @dataclass
 class Project:
-    id: int
-    project_key: UUID
+    project_id: UUID
     title: str
     created_at: datetime
     status: ProjectStatus = field(default=ProjectStatus.ACTIVE)
