a18964a6b020f5a79b441eb61b12d6c9e9b5b503
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-14 23:48:17 -0300

    Changes "GetProjectDetailsPage" to receive "project_id" instead "project_key" as a path parameter

--- kamui/entrypoints/web/project/get_project_details_page.py
+++ kamui/entrypoints/web/project/get_project_details_page.py
@@ -5,8 +5,8 @@ from flask.views import View
 
 
 class GetProjectDetailsPage(View):
-    PATH = "/projects/<uuid:project_key>"
+    PATH = "/projects/<uuid:project_id>"
     methods = ["GET"]
 
-    def dispatch_request(self, project_key: UUID):
+    def dispatch_request(self, project_id: UUID):
         return render_template("project_details_page.html")
