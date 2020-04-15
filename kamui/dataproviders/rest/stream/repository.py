6f32e11828f0eb2617392b9ab8b1aec3573b3a61
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-14 23:36:57 -0300

    Creates "CreateStreamFromKafkaTopicRepository"

---
+++ kamui/dataproviders/rest/stream/repository.py
@@ -0,0 +1,30 @@
+from kamui.dataproviders.rest import client, HttpClient
+from kamui.core.usecase.stream.create_new_stream_from_topic import (
+    CreateNewStreamFromTopicCommand,
+    CreateStreamFromKafkaTopic,
+)
+
+
+class CreateStreamFromKafkaTopicRepository(CreateStreamFromKafkaTopic):
+    # TODO: Make KSQL Server URL configurable
+    def __init__(self):
+        self.__client: HttpClient = client
+        self.__KSQL_SERVER_URL: str = "http://localhost:8088/"
+
+    def __call__(self, creat_new_stream_command: CreateNewStreamFromTopicCommand):
+        query_fields = [
+            f"{field.name} {field.type}" for field in creat_new_stream_command.fields
+        ]
+
+        response = self.__client.post(
+            url=f"{self.__KSQL_SERVER_URL}ksql",
+            payload={
+                "ksql": f"CREATE STREAM {creat_new_stream_command.stream_name} ({', '.join(query_fields)}) WITH (kafka_topic='{creat_new_stream_command.topic_name}', value_format='AVRO');"
+            },
+            headers={
+                "Accept": "application/vnd.ksql.v1+json",
+                "Content-Type": "application/vnd.ksql.v1+json",
+            },
+        )
+
+        return response
