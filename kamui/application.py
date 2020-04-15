c0a7872a7386e80028e34f5b91882ce910790afe
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-15 00:10:19 -0300

    Registers "rest_topic_bp", "rest_stream_bp", "web_stream_bp" blueprints

--- kamui/application.py
+++ kamui/application.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 
 from kamui.entrypoints.rest.health import health_bp
+from kamui.entrypoints.rest.topic import rest_topic_bp
+from kamui.entrypoints.rest.stream import rest_stream_bp
 from kamui.entrypoints.web.home import web_home_bp
 from kamui.entrypoints.web.core import web_core_bp
 from kamui.entrypoints.web.project import web_project_bp
+from kamui.entrypoints.web.stream import web_stream_bp
 
 
 def create_app():
@@ -12,11 +15,14 @@ def create_app():
 
     # Rest
     app.register_blueprint(health_bp)
+    app.register_blueprint(rest_topic_bp)
+    app.register_blueprint(rest_stream_bp)
 
     # Web
     app.register_blueprint(web_home_bp)
     app.register_blueprint(web_core_bp)
     app.register_blueprint(web_project_bp)
+    app.register_blueprint(web_stream_bp)
 
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
