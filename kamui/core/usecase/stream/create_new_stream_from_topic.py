2c2d2187432deeb290aa43a5fb3891be0a1de0f0
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-14 23:09:06 -0300

    Creates "CreateNewStreamFromTopicUsecase"

---
+++ kamui/core/usecase/stream/create_new_stream_from_topic.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import List, Any
+
+
+@dataclass(frozen=True)
+class CreateNewStreamFromTopicCommand:
+    @dataclass(frozen=True)
+    class StreamField:
+        name: str
+        type: str
+
+    stream_name: str
+    fields: List[StreamField]
+    topic_name: str
+
+
+class CreateStreamFromKafkaTopic(ABC):
+    @abstractmethod
+    def __call__(self, creat_new_stream_command: CreateNewStreamFromTopicCommand):
+        pass
+
+
+class CreateNewStreamFromTopicUsecase:
+    def __init__(
+        self, create_stream_from_kafka_topic: CreateStreamFromKafkaTopic
+    ) -> None:
+        self.__create_stream_from_kafka_topic = create_stream_from_kafka_topic
+
+    def __call__(
+        self, create_new_stream_command: CreateNewStreamFromTopicCommand
+    ) -> Any:
+        self.__create_stream_from_kafka_topic(create_new_stream_command)
