959a9f8b14e82dc506820576f2ce35b13540d585
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-19 18:18:16 -0300

    Treats returns from "/topics/<string:topic_name>/schema" REST Entrypoint

--- kamui/entrypoints/rest/topic/get_topic_schema.py
+++ kamui/entrypoints/rest/topic/get_topic_schema.py
@@ -1,6 +1,10 @@
+from typing import Tuple
+
 from flask_restful import Resource
 
 from kamui.configuration.dependency_injection import di_container
+from kamui.core.entity.topic_schema import TopicSchema
+from kamui.core.usecase.failure import FailureDetails
 from kamui.core.usecase.topic.get_topic_schema import GetTopicSchemaUsecase
 
 
@@ -12,4 +16,15 @@ class GetTopicSchemaResource(Resource):
         self.get_topic_schema = di_container.resolve(GetTopicSchemaUsecase)
 
     def get(self, topic_name: str):
-        return self.get_topic_schema(topic_name)
+        return (
+            self.get_topic_schema(topic_name)
+            .map(self.__process_success)
+            .fix(self.__process_failure)
+            .unwrap()
+        )
+
+    def __process_success(self, topic_schema: TopicSchema) -> Tuple[dict, int]:
+        return topic_schema.to_dict(), 200
+
+    def __process_failure(self, failure_details: FailureDetails) -> Tuple[dict, int]:
+        return failure_details.to_dict(), 503
