f0813f9fb885a1cba11eb1326a9e6e7e53801715
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-19 02:10:00 -0300

    Creates "HttpStatus" Enum

--- kamui/dataproviders/rest/__init__.py
+++ kamui/dataproviders/rest/__init__.py
@@ -16,6 +16,23 @@ class HttpMethod(Enum):
     OPTIONS = "OPTIONS"
 
 
+class HttpStatus(Enum):
+    CONTINUE = 100
+    OK = 200
+    CREATED = 201
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    METHOD_NOT_ALLOWED = 405
+    REQUEST_TIMEOUT = 408
+    PRECONDITION_FAILED = 412
+    INTERNAL_SERVER_ERROR = 500
+    BAD_GATEWAY = 502
+    SERVICE_UNAVAILABLE = 503
+    GATEWAY_TIMEOUT = 504
+
+
 class HttpClient:
     # TODO: Make exception treatment
     def __make_request(
