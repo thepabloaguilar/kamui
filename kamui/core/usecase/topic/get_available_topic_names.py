30350a6cef6d692c025b2d2a6f71f393b24c76fd
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-19 02:46:59 -0300

    Changes "GetAvailableTopicNamesUsecase" and "GetTopicNames" classes to return "Result" container on their methods

--- kamui/core/usecase/topic/get_available_topic_names.py
+++ kamui/core/usecase/topic/get_available_topic_names.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 
+from returns.result import Result
+
 from kamui.core.entity.topic import TopicNames
+from kamui.core.usecase.failure import FailureDetails, failure_details
 
 
 class GetTopicNames(ABC):
     @abstractmethod
-    def __call__(self) -> TopicNames:
+    def __call__(self) -> Result[TopicNames, str]:
         pass
 
 
@@ -13,11 +16,14 @@ class GetAvailableTopicNamesUsecase:
     def __init__(self, get_topic_names: GetTopicNames) -> None:
         self.__get_topic_names = get_topic_names
 
-    def __call__(self) -> TopicNames:
+    def __call__(self) -> Result[TopicNames, FailureDetails]:
+        return (
+            self.__get_topic_names()
+            .map(self.__filter_topic_names)
+            .alt(failure_details("Was not possible to get Topic Names"))
+        )
+
+    def __filter_topic_names(self, topic_names: TopicNames) -> TopicNames:
         return TopicNames(
-            [
-                topic_name
-                for topic_name in self.__get_topic_names()
-                if not topic_name.startswith("_")
-            ]
+            [topic_name for topic_name in topic_names if not topic_name.startswith("_")]
         )
