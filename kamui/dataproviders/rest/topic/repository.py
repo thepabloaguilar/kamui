d5cc76f141621f264462bcfb9532016add4e78a9
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-19 02:26:53 -0300

    Changes "GetTopicSchemaVersionsRepository" class to return "Result" container on its methods

--- kamui/dataproviders/rest/topic/repository.py
+++ kamui/dataproviders/rest/topic/repository.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 import orjson
-from returns.result import Result
+from returns.result import Result, Success, Failure
 
 from kamui.core.entity.topic import TopicNames, TopicSchemaVersions
-from kamui.dataproviders.rest import client, HttpClient
+from kamui.dataproviders.rest import client, HttpClient, JsonResponse
 from kamui.core.usecase.topic.get_available_topic_names import GetTopicNames
 from kamui.core.usecase.topic.get_topic_schema import (
     GetTopicSchema,
@@ -45,10 +47,16 @@ class GetTopicSchemaVersionsRepository(GetTopicSchemaVersions):
         self.__client: HttpClient = client
         self.__SCHEMA_REGISTRY_URL: str = "http://localhost:8081/"
 
-    def __call__(self, topic_name: str) -> TopicSchemaVersions:
+    def __call__(self, topic_name: str) -> Result[TopicSchemaVersions, Any]:
         response = self.__client.get(
             url=f"{self.__SCHEMA_REGISTRY_URL}subjects/{topic_name}-value/versions",
             headers={"Content-Type": "application/vnd.schemaregistry.v1+json"},
         )
+        return response.bind(self.__verify_response)
+
+    def __verify_response(
+        self, response: JsonResponse
+    ) -> Result[TopicSchemaVersions, Any]:
         if isinstance(response, list):
-            return TopicSchemaVersions(response)
+            return Success(TopicSchemaVersions(response))
+        return Failure(response)
