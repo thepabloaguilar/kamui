895e817d0c71a43ae1126fe27d690034e0431d75
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-19 02:47:35 -0300

    Changes "GetTopicSchemaUsecase", "GetTopicSchemaVersions", "GetTopicSchema" classes to return "Result" container on their methods

--- kamui/core/usecase/topic/get_topic_schema.py
+++ kamui/core/usecase/topic/get_topic_schema.py
@@ -1,17 +1,22 @@
 from abc import ABC, abstractmethod
+from functools import partial
+from typing import Any
 
-from kamui.core.entity.topic import TopicNames, TopicSchemaVersions
+from returns.result import Result
+
+from kamui.core.entity.topic import TopicSchemaVersions
+from kamui.core.usecase.failure import failure_details, FailureDetails
 
 
 class GetTopicSchema(ABC):
     @abstractmethod
-    def __call__(self, topic_name: str, schema_version: int) -> TopicNames:
+    def __call__(self, schema_version: int, topic_name: str) -> Result[Any, Any]:
         pass
 
 
 class GetTopicSchemaVersions(ABC):
     @abstractmethod
-    def __call__(self, topic_name: str) -> TopicSchemaVersions:
+    def __call__(self, topic_name: str) -> Result[TopicSchemaVersions, Any]:
         pass
 
 
@@ -24,6 +29,12 @@ class GetTopicSchemaUsecase:
         self.__get_topic_schema = get_topic_schema
         self.__get_topic_schema_versions = get_topic_schema_versions
 
-    def __call__(self, topic_name: str):
-        latest_schema_version = max(self.__get_topic_schema_versions(topic_name))
-        return self.__get_topic_schema(topic_name, latest_schema_version)
+    def __call__(self, topic_name: str) -> Result[Any, FailureDetails]:
+        return (
+            self.__get_latest_schema_version(topic_name)
+            .bind(partial(self.__get_topic_schema, topic_name=topic_name))
+            .alt(failure_details("Error on getting Topic Schema"))
+        )
+
+    def __get_latest_schema_version(self, topic_name: str):
+        return self.__get_topic_schema_versions(topic_name).map(max)
