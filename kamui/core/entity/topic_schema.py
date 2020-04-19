92f30dc67e1a962cfab43029e9de63ddf69d1663
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-19 18:12:16 -0300

    Creates "TopicSchema", "TopicSchemaField" classes

--- kamui/core/entity/topic_schema.py
+++ kamui/core/entity/topic_schema.py
@@ -1,4 +1,22 @@
 from typing import NewType, List
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
 
 TopicSchemaVersions = NewType("TopicSchemaVersions", List[int])
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class TopicSchemaField:
+    name: str
+    type: str
+
+
+@dataclass_json
+@dataclass(frozen=True)
+class TopicSchema:
+    type: str
+    name: str
+    namespace: str
+    fields: List[TopicSchemaField]
