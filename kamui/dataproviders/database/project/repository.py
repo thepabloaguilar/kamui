f3f353b0a425313c69137ccb3ff6678ab25a350c
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-24 02:17:58 -0300

    Creates "FindProjectByProjectIdRepository" class

--- kamui/dataproviders/database/project/repository.py
+++ kamui/dataproviders/database/project/repository.py
@@ -1,5 +1,11 @@
 from typing import List
+from uuid import UUID
 
+from returns.maybe import Maybe
+from returns.result import Result, Success
+
+from kamui.core.usecase.failure import FailureDetails
+from kamui.core.usecase.project.get_project_details import FindProjectByProjectId
 from .model import ProjectModel
 from kamui.configuration.database import database_session
 from kamui.core.entity.project import Project
@@ -19,3 +25,12 @@ class CreateNewProjectRepository(CreateNewProject):
 class GetProjectsListRepository(GetProjectsList):
     def __call__(self) -> List[Project]:
         return [project.to_entity() for project in ProjectModel.query.all()]
+
+
+class FindProjectByProjectIdRepository(FindProjectByProjectId):
+    def __call__(self, project_id: UUID) -> Result[Maybe[Project], FailureDetails]:
+        project = ProjectModel.query.filter(
+            ProjectModel.project_id == project_id
+        ).first()
+        maybe_project: Maybe[Project] = Maybe.new(project).map(lambda p: p.to_entity())  # type: ignore
+        return Success(maybe_project)
