608fcb4b1cbd882d83a0d60e55ef856cdf726363
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-24 02:37:03 -0300

    Registers "GetProjectDetailsUsecase", "FindProjectByProjectId & FindProjectByProjectIdRepository", "FindStreamsByProject & FindStreamsByProjectRepository", "SaveStream & SaveStreamRepository" in dependency injection container

--- kamui/configuration/dependency_injection.py
+++ kamui/configuration/dependency_injection.py
@@ -5,6 +5,11 @@ from kamui.core.usecase.project.create_new_project import (
     CreateNewProjectUsecase,
     CreateNewProject,
 )
+from kamui.core.usecase.project.get_project_details import (
+    GetProjectDetailsUsecase,
+    FindProjectByProjectId,
+    FindStreamsByProject,
+)
 from kamui.core.usecase.project.get_projects_list import (
     GetProjectsListUsecase,
     GetProjectsList,
@@ -21,10 +26,16 @@ from kamui.core.usecase.topic.get_topic_schema import (
 from kamui.core.usecase.stream.create_new_stream_from_topic import (
     CreateNewStreamFromTopicUsecase,
     CreateStreamFromKafkaTopic,
+    SaveStream,
 )
 from kamui.dataproviders.database.project.repository import (
     CreateNewProjectRepository,
     GetProjectsListRepository,
+    FindProjectByProjectIdRepository,
+)
+from kamui.dataproviders.database.stream.repository import (
+    SaveStreamRepository,
+    FindStreamsByProjectRepository,
 )
 from kamui.dataproviders.rest.stream.repository import (
     CreateStreamFromKafkaTopicRepository,
@@ -40,14 +51,18 @@ di_container = Container()
 # Dependencies
 di_container.register(CreateNewProject, CreateNewProjectRepository)
 di_container.register(GetProjectsList, GetProjectsListRepository)
+di_container.register(FindProjectByProjectId, FindProjectByProjectIdRepository)
+di_container.register(FindStreamsByProject, FindStreamsByProjectRepository)
 di_container.register(GetTopicNames, GetTopicNamesRepository)
 di_container.register(GetTopicSchema, GetTopicSchemaRepository)
 di_container.register(GetTopicSchemaVersions, GetTopicSchemaVersionsRepository)
 di_container.register(CreateStreamFromKafkaTopic, CreateStreamFromKafkaTopicRepository)
+di_container.register(SaveStream, SaveStreamRepository)
 
 # Usecases
 di_container.register(CreateNewProjectUsecase)
 di_container.register(GetProjectsListUsecase)
+di_container.register(GetProjectDetailsUsecase)
 di_container.register(GetAvailableTopicNamesUsecase)
 di_container.register(GetTopicSchemaUsecase)
 di_container.register(CreateNewStreamFromTopicUsecase)
