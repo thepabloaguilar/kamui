d361844f713f37d3b879db92f058a6f383d7ed56
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-04-28 01:48:42 -0300

    Changes view name from "web_get_create_new_stream_page" to "web_get_create_new_stream_from_topic_page"

--- kamui/entrypoints/web/stream/__init__.py
+++ kamui/entrypoints/web/stream/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+from .get_create_new_stream_from_stream_page import GetCreateNewStreamFromStreamPage
 from .get_create_new_stream_from_topic_page import GetCreateNewStreamFromTopicPage
 
 
@@ -7,5 +8,7 @@ web_stream_bp = Blueprint("web_stream", __name__, template_folder="templates")
 
 web_stream_bp.add_url_rule(
     GetCreateNewStreamFromTopicPage.PATH,
-    view_func=GetCreateNewStreamFromTopicPage.as_view("web_get_create_new_stream_page"),
+    view_func=GetCreateNewStreamFromTopicPage.as_view(
+        "web_get_create_new_stream_from_topic_page"
+    ),
 )
