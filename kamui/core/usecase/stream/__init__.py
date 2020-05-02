6602a96e6806cd1a94d113c15517450e28356014
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-05-02 01:17:57 -0300

    Removes undefined import

--- kamui/core/usecase/stream/__init__.py
+++ kamui/core/usecase/stream/__init__.py
@@ -1,3 +1,2 @@
 from .create_new_stream_from_topic import CreateNewStreamFromTopicUsecase
 from .get_ksql_streams import GetKSQLStreamsUsecase
-from .get_stream_by_name import GetStreamByNameUsecase
