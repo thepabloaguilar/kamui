2f93319061c91e6000abe0be9146b41b6e90fa71
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-05-03 00:20:33 -0300

    Creates "KSQLStreamDetailed" class

--- kamui/core/entity/stream.py
+++ kamui/core/entity/stream.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, NewType
+from typing import List, NewType, Optional, Any
 from uuid import UUID
 
 from dataclasses_json import dataclass_json
@@ -18,6 +18,28 @@ class KSQLStream:
     type: str
     name: str
     format: str
+
+
+@dataclass_json
+@dataclass
+class KSQLStreamDetailed:
+    @dataclass_json
+    @dataclass
+    class KSQLStreamField:
+        @dataclass_json
+        @dataclass
+        class KSQLStreamFieldSchema:
+            type: str
+            fields: Optional[Any]
+
+        name: str
+        schema: KSQLStreamFieldSchema
+
+    name: str
+    fields: List[KSQLStreamField]
+    type: str
+    format: str
+    topic: str
 
 
 StreamList = NewType("StreamList", List[Stream])
