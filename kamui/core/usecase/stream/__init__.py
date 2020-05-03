33c350b0a76081892238d30e427d817b5425e230
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-05-03 00:33:58 -0300

    Creates "GetStreamByNameUsecase" usecase class

--- kamui/core/usecase/stream/__init__.py
+++ kamui/core/usecase/stream/__init__.py
@@ -1,2 +1,3 @@
 from .create_new_stream_from_topic import CreateNewStreamFromTopicUsecase
 from .get_ksql_streams import GetKSQLStreamsUsecase
+from .get_stream_by_name import GetStreamByNameUsecase
---
+++ kamui/core/usecase/stream/get_stream_by_name.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+from returns.result import Result
+
+from kamui.core.entity.stream import KSQLStreamDetailed
+from kamui.core.usecase.failure import BusinessFailureDetails, FailureDetails
+
+
+class GetStreamByName(ABC):
+    @abstractmethod
+    def __call__(self, stream_name: str) -> Result[KSQLStreamDetailed, FailureDetails]:
+        pass
+
+
+class GetStreamByNameUsecase:
+    def __init__(self, get_stream_by_name: GetStreamByName) -> None:
+        self.__get_stream_by_name = get_stream_by_name
+
+    def __call__(
+        self, stream_name: str
+    ) -> Result[KSQLStreamDetailed, BusinessFailureDetails]:
+        return self.__get_stream_by_name(stream_name).alt(
+            lambda failure: BusinessFailureDetails(
+                reason="NON_BUSINESS_RULE_CAUSE",
+                failure_message="Was not possible to get Stream",
+                failure_due=failure,
+            )
+        )
