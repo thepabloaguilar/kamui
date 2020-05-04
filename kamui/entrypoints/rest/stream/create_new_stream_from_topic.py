fe7b420ab69518f126ce9649daf61679e6a051b6
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-05-04 01:18:55 -0300

    Changes "CreateNewStreamFromTopicResource" class to receive "CreateNewStreamCommandVO" from request body

--- kamui/entrypoints/rest/stream/create_new_stream_from_topic.py
+++ kamui/entrypoints/rest/stream/create_new_stream_from_topic.py
@@ -1,10 +1,13 @@
+from dataclasses import dataclass
 from typing import Any, Dict, Tuple, Union
 
 from flask import request
 from flask_restful import Resource
 from dataclasses_json import dataclass_json
+from returns.result import Result
 
 from kamui.configuration.dependency_injection import di_container
+from kamui.core.entity.stream import Stream
 from kamui.core.usecase.failure import (
     DataProviderFailureDetails,
     BusinessFailureDetails,
@@ -15,25 +18,36 @@ from kamui.core.usecase.stream.create_new_stream_from_topic import (
 )
 
 
+@dataclass_json
+@dataclass(frozen=True)
+class CreateNewStreamCommandVO:
+    stream_origin: str
+    create_new_stream_command: CreateNewStreamCommand
+
+
 class CreateNewStreamFromTopicResource(Resource):
     API_PATH = "/streams/"
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.__create_new_stream_from_topic = di_container.resolve(
-            CreateNewStreamFromTopicUsecase
+        self.__create_new_stream_from_topic: CreateNewStreamFromTopicUsecase = (
+            di_container.resolve(CreateNewStreamFromTopicUsecase)
         )
 
     def post(self) -> Union[Tuple[Any, int], Tuple[Dict[str, Any], int]]:
-        deserializable_body_class = dataclass_json(CreateNewStreamCommand)
-        return (  # type: ignore
-            self.__create_new_stream_from_topic(
-                deserializable_body_class.from_dict(request.json)
-            )
+        body_request = CreateNewStreamCommandVO.from_dict(request.json)  # type: ignore
+        return (
+            self.__create_new_stream(body_request)
             .map(self.__process_success)
             .fix(self.__process_failure)
             .unwrap()
         )
+
+    def __create_new_stream(
+        self, command: CreateNewStreamCommandVO
+    ) -> Result[Stream, BusinessFailureDetails]:
+        # TODO: Verify all cases of `command.stream_origin`
+        return self.__create_new_stream_from_topic(command.create_new_stream_command)
 
     def __process_success(self, something: Any) -> Tuple[Any, int]:
         return something, 201
