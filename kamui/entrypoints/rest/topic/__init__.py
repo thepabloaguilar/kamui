c7c59439d7519f87e953efdac604c81b6b2ed045
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-05-24 13:33:21 -0300

    Creates "GetTopicNamesResource" class

--- kamui/entrypoints/rest/topic/__init__.py
+++ kamui/entrypoints/rest/topic/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 
 from .get_topic_schema import GetTopicSchemaResource
+from .get_topic_names import GetTopicNamesResource
 
 
 rest_topic_bp = Blueprint("rest_topic", __name__)
 rest_topic_api = Api(rest_topic_bp, prefix="/api")
 
 rest_topic_api.add_resource(GetTopicSchemaResource, GetTopicSchemaResource.API_PATH)
+rest_topic_api.add_resource(GetTopicNamesResource, GetTopicNamesResource.API_PATH)
---
+++ kamui/entrypoints/rest/topic/get_topic_names.py
@@ -0,0 +1,34 @@
+from typing import Any, Tuple
+
+from flask_restful import Resource
+
+from kamui.configuration.dependency_injection import di_container
+from kamui.core.entity.topic import TopicNames
+from kamui.core.usecase.failure import FailureDetails
+from kamui.core.usecase.topic import GetAvailableTopicNamesUsecase
+
+
+class GetTopicNamesResource(Resource):
+    API_PATH = "/topics"
+
+    def __init__(self, *args: Any, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.__get_available_topic_names = di_container.resolve(
+            GetAvailableTopicNamesUsecase
+        )
+
+    def dispatch_request(self) -> str:
+        available_topic_names = self.__get_available_topic_names()
+        return (
+            available_topic_names.map(self.__process_success_return)
+            .fix(self.__process_failure_return)
+            .unwrap()
+        )
+
+    def __process_success_return(
+        self, available_topic_names: TopicNames
+    ) -> Tuple[TopicNames, int]:
+        return available_topic_names, 200
+
+    def __process_failure_return(self, failure_details: FailureDetails) -> Any:
+        return {"status": "error"}, 503
