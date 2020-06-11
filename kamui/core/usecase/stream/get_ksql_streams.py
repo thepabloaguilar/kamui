caf70f0dc68c3558892c15be5959d52bc0987598
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-11 13:58:14 -0300

    Fixes parameter and variable names from "GetKSQLStreamsUsecase" class

--- kamui/core/usecase/stream/get_ksql_streams.py
+++ kamui/core/usecase/stream/get_ksql_streams.py
@@ -14,11 +14,11 @@ class GetKSQLStreams(ABC):
 
 
 class GetKSQLStreamsUsecase:
-    def __init__(self, get_stream_names: GetKSQLStreams) -> None:
-        self.__get_stream_names = get_stream_names
+    def __init__(self, get_ksql_streams: GetKSQLStreams) -> None:
+        self.__get_ksql_streams = get_ksql_streams
 
     def __call__(self) -> Result[List[KSQLStream], BusinessFailureDetails]:
-        return self.__get_stream_names().alt(
+        return self.__get_ksql_streams().alt(
             lambda failure: BusinessFailureDetails(
                 reason="NON_BUSINESS_RULE_CAUSE",
                 failure_message="An error occurred while getting the stream list",
