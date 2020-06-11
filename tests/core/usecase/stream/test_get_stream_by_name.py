93106b4d5bd198169f4dc01032d9229baa3b1a9c
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-11 02:22:39 -0300

    Creates tests for "GetStreamByNameUsecase" class

---
+++ tests/core/usecase/stream/test_get_stream_by_name.py
@@ -0,0 +1,58 @@
+from unittest.mock import Mock
+
+import pytest
+from returns.result import Failure, Result, Success
+
+from kamui.core.entity.stream import KSQLStreamDetailed
+from kamui.core.usecase.failure import FailureDetails, BusinessFailureDetails
+from kamui.core.usecase.stream.get_stream_by_name import (
+    GetStreamByName,
+    GetStreamByNameUsecase,
+)
+
+
+@pytest.fixture(scope="function")
+def get_stream_by_name() -> Mock:
+    return Mock(spec=GetStreamByName)
+
+
+@pytest.fixture(scope="function")
+def get_stream_by_name_usecase(get_stream_by_name: Mock) -> GetStreamByNameUsecase:
+    return GetStreamByNameUsecase(get_stream_by_name)
+
+
+def test_should_return_ksql_stream_detailed_correctly(
+    get_stream_by_name_usecase: GetStreamByNameUsecase, get_stream_by_name: Mock
+) -> None:
+    ksql_stream_detailed = KSQLStreamDetailed(
+        name="SUCCESS_TEST_STREAM",
+        fields=[],
+        type="STREAM",
+        format="AVRO",
+        topic="topic",
+    )
+    get_stream_by_name.return_value = Success(ksql_stream_detailed)
+
+    actual = get_stream_by_name_usecase("SUCCESS_TEST_STREAM")
+
+    get_stream_by_name.assert_called_once()
+    get_stream_by_name.assert_called_with("SUCCESS_TEST_STREAM")
+    assert isinstance(actual, Result.success_type)
+    assert isinstance(actual.unwrap(), KSQLStreamDetailed)
+    assert ksql_stream_detailed == actual.unwrap()
+
+
+def test_should_return_failure_when_an_error_occurred_while_getting_stream_names(
+    get_stream_by_name_usecase: GetStreamByNameUsecase, get_stream_by_name: Mock
+) -> None:
+    failure = Failure(FailureDetails(reason="FAILURE_TEST"))
+    get_stream_by_name.return_value = failure
+
+    actual = get_stream_by_name_usecase("MY_STREAM_NAME")
+
+    get_stream_by_name.assert_called_once()
+    get_stream_by_name.assert_called_with("MY_STREAM_NAME")
+    assert isinstance(actual, Result.failure_type)
+    assert isinstance(actual.failure(), BusinessFailureDetails)
+    assert "NON_BUSINESS_RULE_CAUSE" == actual.failure().reason
+    assert failure.failure() == actual.failure().failure_due
