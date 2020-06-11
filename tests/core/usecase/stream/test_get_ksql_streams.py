d593214bf3e99ed3e6a770193c5c5b98c0c99447
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-11 14:15:00 -0300

    Creates tests for "GetKSQLStreamsUsecase" class

---
+++ tests/core/usecase/stream/test_get_ksql_streams.py
@@ -0,0 +1,54 @@
+from unittest.mock import Mock
+
+import pytest
+from returns.result import Failure, Result, Success
+
+from kamui.core.entity.stream import KSQLStream
+from kamui.core.usecase.failure import FailureDetails, BusinessFailureDetails
+from kamui.core.usecase.stream.get_ksql_streams import (
+    GetKSQLStreams,
+    GetKSQLStreamsUsecase,
+)
+
+
+@pytest.fixture(scope="function")
+def get_ksql_streams() -> Mock:
+    return Mock(spec=GetKSQLStreams)
+
+
+@pytest.fixture(scope="function")
+def get_ksql_streams_usecase(get_ksql_streams: Mock) -> GetKSQLStreamsUsecase:
+    return GetKSQLStreamsUsecase(get_ksql_streams)
+
+
+def test_should_return_list_of_ksql_stream_correctly(
+    get_ksql_streams_usecase: GetKSQLStreamsUsecase, get_ksql_streams: Mock
+) -> None:
+    ksql_streams_list = [
+        KSQLStream("STREAM", "STREAM_ONE", "AVRO"),
+        KSQLStream("STREAM", "STREAM_TWO", "JSON"),
+        KSQLStream("STREAM", "STREAM_THREE", "AVRO"),
+    ]
+    get_ksql_streams.return_value = Success(ksql_streams_list)
+
+    actual = get_ksql_streams_usecase()
+
+    get_ksql_streams.assert_called_once()
+    assert isinstance(actual, Result.success_type)
+    assert isinstance(actual.unwrap(), list)
+    assert ksql_streams_list == actual.unwrap()
+
+
+def test_should_return_failure_when_an_error_occurred_while_getting_ksql_streams(
+    get_ksql_streams_usecase: GetKSQLStreamsUsecase, get_ksql_streams: Mock
+) -> None:
+    failure = Failure(FailureDetails(reason="FAILURE_TEST"))
+    get_ksql_streams.return_value = failure
+
+    actual = get_ksql_streams_usecase()
+
+    get_ksql_streams.assert_called_once()
+    assert isinstance(actual, Result.failure_type)
+    assert isinstance(actual.failure(), BusinessFailureDetails)
+    assert "NON_BUSINESS_RULE_CAUSE" == actual.failure().reason
+    assert failure.failure() == actual.failure().failure_due
