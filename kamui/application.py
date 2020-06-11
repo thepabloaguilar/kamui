520e2e1f863d7061ad55e708428dc166770c5052
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-11 16:21:21 -0300

    Removes "entrypoints.web" module

--- kamui/application.py
+++ kamui/application.py
@@ -5,10 +5,6 @@ from kamui.entrypoints.rest.health import health_bp
 from kamui.entrypoints.rest.topic import rest_topic_bp
 from kamui.entrypoints.rest.stream import rest_stream_bp
 from kamui.entrypoints.rest.project import rest_project_bp
-from kamui.entrypoints.web.home import web_home_bp
-from kamui.entrypoints.web.core import web_core_bp
-from kamui.entrypoints.web.project import web_project_bp
-from kamui.entrypoints.web.stream import web_stream_bp
 
 
 def create_app() -> Flask:
@@ -19,12 +15,6 @@ def create_app() -> Flask:
     app.register_blueprint(rest_topic_bp)
     app.register_blueprint(rest_stream_bp)
     app.register_blueprint(rest_project_bp)
-
-    # Web
-    app.register_blueprint(web_home_bp)
-    app.register_blueprint(web_core_bp)
-    app.register_blueprint(web_project_bp)
-    app.register_blueprint(web_stream_bp)
 
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
--- kamui/entrypoints/web/core/not_found_page.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask.views import View
-
-
-class NotFoundPage(View):
-    PATH = "/not-found"
-
-    def dispatch_request(self) -> str:  # type: ignore
-        return render_template("not_found.html")
