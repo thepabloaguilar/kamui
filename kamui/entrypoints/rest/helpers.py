b5297cb7bc85734c1d3544d231576c4a992cf094
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-14 02:38:33 -0300

    Creates "unwrap_result_response" decorator

--- kamui/entrypoints/rest/helpers.py
+++ kamui/entrypoints/rest/helpers.py
@@ -1,8 +1,12 @@
 from functools import wraps
-from typing import Callable, Tuple, Any
+from typing import Callable, Tuple, Any, TypeVar
 
 import orjson
 from flask import Response
+from returns.result import Result
+
+_SuccessType = TypeVar("_SuccessType")
+_FailureType = TypeVar("_FailureType")
 
 
 def json_response(function: Callable[..., Tuple[Any, int]]) -> Callable[..., Response]:
@@ -16,3 +20,26 @@ def json_response(function: Callable[..., Tuple[Any, int]]) -> Callable[..., Res
         )
 
     return decorator
+
+
+def unwrap_result_response(
+    success_status_code: int,
+) -> Callable[
+    [Callable[..., Result[_SuccessType, _FailureType]]], Callable[..., Tuple[Any, int]]
+]:
+    def _unwrap_result_response(
+        function: Callable[..., Result[_SuccessType, _FailureType]]
+    ) -> Callable[..., Tuple[Any, int]]:
+        @wraps(function)
+        def decorator(*args: Any, **kwargs: Any) -> Tuple[Any, int]:
+            # TODO: Improve failure treatment
+            return (
+                function(*args, **kwargs)
+                .map(lambda response: (response, success_status_code))
+                .alt(lambda failure: (failure, 503))
+                .unwrap()
+            )
+
+        return decorator
+
+    return _unwrap_result_response
