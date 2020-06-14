9275047b5565f19ce1c8f57202428a054d57646c
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-14 14:41:46 -0300

    Makes failure classes use pydantic dataclass decorator

--- kamui/core/usecase/failure.py
+++ kamui/core/usecase/failure.py
@@ -1,23 +1,19 @@
-from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
-from dataclasses_json import dataclass_json
+from pydantic.dataclasses import dataclass
 
 
-@dataclass_json
 @dataclass
 class FailureDetails:
     reason: str
 
 
-@dataclass_json
 @dataclass
 class BusinessFailureDetails(FailureDetails):
     failure_message: str
     failure_due: Optional[Any] = None
 
 
-@dataclass_json
 @dataclass
 class DataProviderFailureDetails(FailureDetails):
     dataprovider_type: str
