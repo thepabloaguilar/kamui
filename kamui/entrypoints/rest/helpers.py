c9280e17c04d80e04b24865b122361c7067b373f
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2020-06-14 16:53:42 -0300

    Creates "parse_request_body" decorator

--- kamui/entrypoints/rest/helpers.py
+++ kamui/entrypoints/rest/helpers.py
@@ -2,8 +2,11 @@ from functools import wraps
 from typing import Callable, Tuple, Any, TypeVar
 
 import orjson
-from flask import Response
-from returns.result import Result
+from flask import Response, request
+from pydantic import ValidationError
+from returns.result import Result, Success, Failure
+
+from kamui.core.usecase.failure import NewFailureDetails
 
 _SuccessType = TypeVar("_SuccessType")
 _FailureType = TypeVar("_FailureType")
@@ -18,6 +21,39 @@ def json_response(function: Callable[..., Tuple[Any, int]]) -> Callable[..., Res
         return _create_response(return_, status_code)
 
     return decorator
+
+
+def parse_request_body(
+    clazz: Any,
+) -> Callable[
+    [Callable[..., Result[_SuccessType, _FailureType]]],
+    Callable[..., Result[_SuccessType, _FailureType]],
+]:
+    def _parse_request_body(
+        function: Callable[..., Result[_SuccessType, _FailureType]]
+    ) -> Callable[..., Result[_SuccessType, _FailureType]]:
+        @wraps(function)
+        def decorator(*args: Any, **kwargs: Any) -> Result[_SuccessType, _FailureType]:
+            try:
+                json_body = orjson.loads(request.data)
+                request_body = Success(clazz(**json_body))
+            except (ValidationError, TypeError) as error:
+                raw_errors = getattr(error, "raw_errors", [])
+                errors = {e._loc: e.exc.msg_template for e in raw_errors}
+                failure = NewFailureDetails(
+                    reason="BODY_PARSE_ERROR",
+                    failure_message="Body request has to match exactly as the body schema",  # noqa: E501
+                    attributes={
+                        "expected_body_schema": clazz.__pydantic_model__.schema(),
+                        "field_errors": errors,
+                    },
+                )
+                request_body = Failure(_create_response(failure, 400))
+            return function(*args, **kwargs, request_body=request_body)
+
+        return decorator
+
+    return _parse_request_body
 
 
 def unwrap_result_response(
